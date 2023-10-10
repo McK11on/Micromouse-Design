c472f4b31e38f87c088b38aaecf451633ae8c305
McK11on <cristianmaquimejia@gmail.com>
2023-10-10 17:29:00 -0500

    example

---
+++ BFSmaze_solver.py
@@ -0,0 +1,71 @@
+# This file is in charge of solving the maze by the BFS (Breadth First Search) method
+# ------------------------------------------------------------------------------------------------------------ #
+from pyamaze import maze,agent,textLabel,COLOR
+from collections import deque
+
+
+# SearchAlgorithm: BFS
+def BFS(m,start=None):
+    if start is None:
+        start=(m.rows,m.cols)
+    frontier = deque()
+    frontier.append(start)
+    bfsPath = {}
+    explored = [start]
+    bSearch=[]
+
+    while len(frontier)>0:
+        currCell=frontier.popleft()
+        if currCell==m._goal:
+            break
+        for d in 'ESNW':
+            if m.maze_map[currCell][d]==True:
+                if d=='E':
+                    childCell=(currCell[0],currCell[1]+1)
+                elif d=='W':
+                    childCell=(currCell[0],currCell[1]-1)
+                elif d=='S':
+                    childCell=(currCell[0]+1,currCell[1])
+                elif d=='N':
+                    childCell=(currCell[0]-1,currCell[1])
+                if childCell in explored:
+                    continue
+                frontier.append(childCell)
+                explored.append(childCell)
+                bfsPath[childCell] = currCell
+                bSearch.append(childCell)
+    # print(f'{bfsPath}')
+    fwdPath={}
+    cell=m._goal
+    while cell!=start:
+        fwdPath[bfsPath[cell]]=cell
+        cell=bfsPath[cell]
+    return bSearch,bfsPath,fwdPath
+
+
+if __name__=='__main__':
+    # Configuring the maze
+    m=maze(10,10)
+
+    ini_cell= (1,1) # Start Cell
+    goal_cell = (10,2) # Goal Cell
+
+    m.CreateMaze(goal_cell[0],goal_cell[1],loopPercent=100) #,loadMaze="testingMaze.csv"
+
+    # BFS algorithm
+    bSearch,bfsPath,fwdPath=BFS(m,ini_cell)
+
+    # Agents
+    a=agent(m,ini_cell[0],ini_cell[1],footprints=True,color=COLOR.green,shape='square') # Search agent
+    c=agent(m,goal_cell[0],goal_cell[1],goal=(ini_cell),footprints=True,color=COLOR.cyan,shape='square',filled=True) # Return agent
+    b=agent(m,ini_cell[0],ini_cell[1],color=COLOR.yellow,shape='arrow') # Forward agent
+
+    # Trace the path
+    m.tracePath({a:bSearch},showMarked=True,delay=100)
+    m.tracePath({c:bfsPath},delay=100)
+    m.tracePath({b:fwdPath},delay=100)
+
+    l=textLabel(m,'BFS Final Path Length',len(fwdPath)+1)
+    l=textLabel(m,'BFS Search Length',len(bSearch))
+
+    m.run()
